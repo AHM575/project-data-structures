d51b746fb54248d3539168c29efc92751f14c51e
Malak <s-malak.wagih@zewailcity.edu.eg>
2025-11-20 21:47:41 +0200

    queue.h

--- queue.h
+++ queue.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "Common.h"
+
+class Queue {
+private:
+    Vehicle* head;
+    Vehicle* tail;
+    int cnt;
+public:
+    Queue();
+    ~Queue();
+    void enqueue(Vehicle* v);
+    Vehicle* dequeue();
+    bool isEmpty();
+    int size();
+    void print();
+    Vehicle* searchByID(VID id);
+    bool removeByID(VID id);
+};
+
+#endif
+
+
