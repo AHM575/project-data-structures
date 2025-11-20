dd391641b8e682983b944ae5cd15999009c33a47
Malak <s-malak.wagih@zewailcity.edu.eg>
2025-11-20 21:50:54 +0200

    PriorityQueue.h

--- ) PriorityQueue.h
+++ ) PriorityQueue.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+#include "Common.h"
+
+class PriorityQueue {
+private:
+    Vehicle* head;
+    int cnt;
+public:
+    PriorityQueue();
+    ~PriorityQueue();
+    void insertSorted(Vehicle* v); // larger priority first
+    Vehicle* removeFront();
+    Vehicle* peek();
+    bool isEmpty();
+    int size();
+    Vehicle* searchByID(VID id);
+    bool removeByID(VID id);
+    void print();
+};
+
+#endif
+
+
