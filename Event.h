7583530a446555d2dd3d7f8dfce0bea8037520ee
Malak <s-malak.wagih@zewailcity.edu.eg>
2025-11-20 22:21:22 +0200

    Event.h

--- Event.h
+++ Event.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <iostream>
+using namespace std;
+
+class Event {
+private:
+    int eID;
+    string eType;
+    int time;
+    int vehicleID;
+
+public:
+    Event* next;
+
+    Event();
+    Event(int id, string type, int t, int vid = -1);
+
+    int getEventID();
+    string getEventType();
+    int getTime();
+    int getVehicleID();
+
+    void printEvent();
+};
+
+#endif
