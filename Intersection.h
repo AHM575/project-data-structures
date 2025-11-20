6bf4b154a9636c58f00136cfa034df1a8876508e
Malak <s-malak.wagih@zewailcity.edu.eg>
2025-11-20 22:24:01 +0200

    Intersection.h

--- Intersection.h
+++ Intersection.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+#include "Vehicle.h"
+
+class Intersection {
+public:
+    int id;
+    int lanesCount;
+
+    Vehicle** laneQueues;
+    bool* blocked;
+
+    Intersection() {
+        id = 0;
+        lanesCount = 0;
+        laneQueues = NULL;
+        blocked = NULL;
+    }
+
+    Intersection(int _id, int _lanes) {
+        id = _id;
+        lanesCount = _lanes;
+
+        laneQueues = new Vehicle*[lanesCount];
+        blocked = new bool[lanesCount];
+
+        for (int i = 0; i < lanesCount; i++) {
+            laneQueues[i] = NULL;
+            blocked[i] = false;
+        }
+    }
+};
+
+#endif
